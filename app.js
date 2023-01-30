0e77a2eba99d3bcdf7184465c56a2d8776abf9f0
Uchot Jitpaisarnsook_Telogic <uchoj108@TELOG.postbox.in.th>
2023-01-30 18:49:04 +0700

    Implement stb

--- app.js
+++ app.js
@@ -7,56 +7,435 @@ const port = process.env.PORT || 3000;
 const baseDir = __dirname + "/";
 const debug = true;
 
+const normalText = 'Hello World';
+
 function logDebugMessage(message) {
-  if (debug) {
+  if ((process.env.NODE_ENV !== 'prod') && (debug)) {
     console.log(message);
   }
+}
+
+function runPromise(promise) {
+  return promise.then(data => {
+     return [null, data];
+  })
+  .catch(err => [err]);
+}
+
+async function processMergeCard(page, item, makeDefault, objResult) {
+  // Init Card Info
+  let card = {
+    orderID: item.orderID,
+    cardNumber: item.cardNumber,
+    cardCode: item.cardCode,
+    balance: item.balance,
+    cardBalanceBefore: '',
+    cardBalanceAfter: ''
+  }
+  logDebugMessage(`==== Process Card: ${card.cardNumber} `);
+
+  try {
+    let error, result;
+
+    // Click 'Add a Card'
+    await page.$eval('#card_add', el => el.click());
+    await page.waitForNetworkIdle({idleTime: 500});
+
+    // Fill Info
+    // Card Nick Name
+    await page.type('#CardNickName', card.orderID);
+    // Card Number
+    await page.type('#CardNumber', card.cardNumber);
+    // Card Security Code (CSC)
+    await page.type('#CardCSC', card.cardCode);
+
+    // Click btn Add Card
+    await page.$eval('button[type=submit]', el => el.click());
+    await page.waitForNetworkIdle({idleTime: 500});
+
+    [error, result] = await runPromise(page.$eval('#card-properties > span.balance.numbers', el => el.innerHTML));
+    if (error) {
+      // Card Error
+      logDebugMessage(`**** Error Before Transfer Funds ****`);
+      objResult.listFail.push(card);
+      return false;
+    }
+
+    // Get Balance of this card: Before Transfer
+    let txtBalance = result;
+    let str = txtBalance.split(' ');
+    card.cardBalanceBefore = str[0];
+    logDebugMessage(`cardBalanceBefore: ${card.cardBalanceBefore}`);
+
+    // No Value in Card
+    if (parseInt(card.cardBalanceBefore) === 0) {
+      logDebugMessage(`**** Duplicated or No Value ****`);
+      card.cardBalanceAfter = 'NO_VAL';
+      objResult.listFail.push(card);
+
+      // Click Remove Card
+      await page.$eval('#btn06', el => el.click());
+      await page.waitForNetworkIdle({idleTime: 500});
+
+      // Click Submit
+      await page.$eval('#Button1', el => el.click());
+      await page.waitForNetworkIdle({idleTime: 500});
+
+      return false;
+    }
+
+    // Only First Time, Set it as default
+    if (makeDefault) {
+      // Click Set Default Card
+      await page.$eval('#btn08', el => el.click());
+      await page.waitForNetworkIdle({idleTime: 500});
+
+      logDebugMessage(`** Default Card **`);
+      card.cardBalanceAfter = 'DEFAULT';
+      objResult.amountTotal = objResult.amountTotal + parseInt(card.cardBalanceBefore);
+      objResult.listSuccess.push(card);
+      objResult.listMergedCard.push(card);
+      return true;
+    }
+
+    // Click Transfer Funds
+    await page.$eval('#btn03', el => el.click());
+    await page.waitForNetworkIdle({idleTime: 500});
+
+    // Click Submit
+    await page.$eval('#reload_submit', el => el.click());
+    await page.waitForNetworkIdle({idleTime: 500});
+
+    [error, result] = await runPromise(page.$eval('#card-properties > span.balance.numbers', el => el.innerHTML));
+    if (error) {
+      // Card Error
+      logDebugMessage(`**** Error After Transfer Funds ****`);
+      objResult.listFail.push(card);
+      return false;
+    }
+
+    // Get Balance of this card: After Transfer
+    txtBalance = result;
+    str = txtBalance.split(' ');
+    card.cardBalanceAfter = str[0];
+    logDebugMessage(`cardBalanceAfter: ${card.cardBalanceAfter}`);
+  
+    // Write Output
+    objResult.amountTotal = objResult.amountTotal + parseInt(card.cardBalanceBefore);
+    objResult.listSuccess.push(card);
+
+    // Transfer Success
+    if (card.cardBalanceBefore !== card.cardBalanceAfter) {
+
+      // Click Remove Card
+      await page.$eval('#btn06', el => el.click());
+      await page.waitForNetworkIdle({idleTime: 500});
+
+      // Click Submit
+      await page.$eval('#Button1', el => el.click());
+      await page.waitForNetworkIdle({idleTime: 500});
+
+      return true;
+    }
+
+    // Transfer Failed, then set it as default
+    // Click Set Default Card
+    await page.$eval('#btn08', el => el.click());
+    await page.waitForNetworkIdle({idleTime: 500});
+
+    logDebugMessage(`** Default Card **`);
+    objResult.listMergedCard.push(card);
+  } catch (error) {
+    // Do something before throw error
+    console.log(`**** Error on processMergeCard ****`);
+
+    // Show Result
+    console.log(`Process amount: ${objResult.amountTotal}`);
+    if (objResult.listFail.length > 0) {
+      console.log(`*** Failed card ***`);
+      objResult.listFail.map((card) => {
+        console.log(Object.values(card).toString());
+      })
+    }
+
+    throw error;
+  }
+
+  return true;
 }
 
 /**
  * Main Program - Start Server
  */
 const server = http.createServer(async (req, res) => {
-  if (req.url === '/favicon.ico') {
-    res.writeHead(200);
-    res.end('');
-  }
+  const { headers, method } = req;
 
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    let htmlText = 'Hello World';
-    res.end(htmlText);
-  }
+  // GET
+  if (method === 'GET') {
+    if (req.url === '/favicon.ico') {
+      res.writeHead(200);
+      res.end('');
+      return;
+    }
 
-  if (req.url.includes('stb')) {
-    const queryObject = url.parse(req.url,true).query;
-    logDebugMessage(JSON.stringify(queryObject, null, 2));
+    if (req.url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+      res.end(normalText);
+      return;
+    }
 
-    let browser = null;
-    try {
-      if (process.env.NODE_ENV === 'prod') {
-        browser = await puppeteer.launch({ headless: true });
-      } else {
-        browser = await puppeteer.launch({ headless: false, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' });
+    if (req.url.includes('stb')) {
+      const queryObject = url.parse(req.url, true).query;
+      logDebugMessage(JSON.stringify(queryObject, null, 2));
+
+      const email = decodeURI(queryObject['email']);
+      const password = decodeURI(queryObject['password']);
+      logDebugMessage(`email: ${email}, password: ${password}`);
+
+      // No Email and Password
+      if (!(email && password)) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+        res.end(normalText);
+        return;
       }
-      const page = await browser.newPage();
   
-      await page.goto('http://www.example.com/', {waitUntil: 'networkidle2'});
-      const screenshot = await page.screenshot();
-  
-      res.end(screenshot, 'binary');
-    } catch (error) {
-      if (!res.headersSent) {
+      let browser = null;
+      try {
+        if (process.env.NODE_ENV === 'prod') {
+          browser = await puppeteer.launch({
+            headless: true,
+            ignoreHTTPSErrors: true,
+            slowMo: 0,
+            args: [
+              '--disable-gpu',
+              '--disable-dev-shm-usage',
+              '--disable-setuid-sandbox',
+              '--no-first-run',
+              '--no-sandbox',
+              '--no-zygote',
+              '--window-size=1280,720',
+            ]
+          });
+        } else {
+          browser = await puppeteer.launch({
+            headless: false,
+            ignoreHTTPSErrors: true,
+            slowMo: 0,
+            args: [
+              '--disable-gpu',
+              '--disable-dev-shm-usage',
+              '--disable-setuid-sandbox',
+              '--no-first-run',
+              '--no-sandbox',
+              '--no-zygote',
+              '--window-size=1280,720',
+            ],
+            executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+          });
+        }
+        const page = await browser.newPage();
+
+        await page.setViewport({ width: 1280, height: 720 });
+
+        // Block images, videos, fonts from downloading
+        await page.setRequestInterception(true);
+        page.on('request', (interceptedRequest) => {
+          const blockResources = ['script', 'stylesheet', 'image', 'media', 'font'];
+          if (blockResources.includes(interceptedRequest.resourceType())) {
+            interceptedRequest.abort();
+          } else {
+            interceptedRequest.continue();
+          }
+        });
+        await page.goto('https://www.starbuckscardth.in.th/Authorize', {waitUntil: 'networkidle2'});
+
+        // Login by Fill User / Password
+        await page.type('#Email', email);
+        await page.type('#Password', password);
+        await page.$eval('button[type=submit]', el => el.click());
+        await page.waitForNetworkIdle({idleTime: 500});
+
+        const screenshot = await page.screenshot();
+    
+        res.end(screenshot, 'binary');
+      } catch (error) {
         res.statusCode = 400;
         res.end(error.message);
+      } finally {
+        if (browser) {
+          browser.close();
+        }
       }
-    } finally {
-      if (browser) {
-        browser.close();
-      }
+
+      return;
     }
   }
+
+  // POST
+  if (method === 'POST') {
+    if (req.url === '/testPost') {
+      let body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', async () => {
+        body = Buffer.concat(body).toString();
+        const payload = JSON.parse(body);
+        // logDebugMessage(payload)
+
+        let startTime = new Date();
+        const email = payload['email'];
+        const password = payload['password'];
+        const command = payload['command']; // Merge or Split
+        const dataItems = payload['dataItems'];
+        
+        // logDebugMessage(`email: ${email}, password: ${password}`);
+
+        // No Email and Password
+        if (!(email && password)) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+          res.end(normalText);
+          return;
+        }
+
+        // TODO: Get dataItem from another source
+        // Validate dataItems
+        let inputJSON = false
+        if (dataItems && Array.isArray(dataItems)) {
+          let item = dataItems[0];
+          if (typeof item === 'object' && item !== null) {
+            inputJSON = true
+          }
+        } else {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+          res.end(normalText);
+          return;
+        }
+
+        let browser = null;
+        try {
+          if (process.env.NODE_ENV === 'prod') {
+            browser = await puppeteer.launch({
+              headless: true,
+              ignoreHTTPSErrors: true,
+              slowMo: 0,
+              args: [
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote'
+              ]
+            });
+          } else {
+            browser = await puppeteer.launch({
+              headless: false,
+              ignoreHTTPSErrors: true,
+              slowMo: 0,
+              args: [
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote'
+              ],
+              executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+            });
+          }
+          const page = await browser.newPage();
+          await page.goto('https://www.starbuckscardth.in.th/Authorize', {waitUntil: 'networkidle2'});
+  
+          // Login by Fill User / Password
+          await page.type('#Email', email);
+          await page.type('#Password', password);
+          await page.$eval('button[type=submit]', el => el.click());
+          await page.waitForNetworkIdle({idleTime: 500});
+
+          // Click 'Card'
+          await page.$eval('input[type=submit]', el => el.click());
+          await page.waitForNetworkIdle({idleTime: 500});
+
+          // Loop for Add Card
+          let makeDefault = true; // Flag for Make Default on first item
+          let processResult = false;
+          let objResult = {
+            amountTotal: 0,
+            listMergedCard: [],
+            listSuccess: [],
+            listFail: []
+          }
+          for(let i = 0; i < dataItems.length; i++) {
+            if (inputJSON) {
+              const item = dataItems[i];
+              processResult = await processMergeCard(page, item, makeDefault, objResult);
+            } else {
+              // Convert each CSV to JSON
+              const line = dataItems[i];
+              if (line != '') {
+                const data = line.split(',')
+                let item = {
+                  orderID: i.toString().padStart(3, '0'),
+                  cardNumber: data[0],
+                  cardCode: data[1],
+                  balance: data[2]
+                }
+                processResult = await processMergeCard(page, item, makeDefault, objResult);
+              }
+            }
+
+            if (processResult && makeDefault) {
+              makeDefault = false;
+            }
+          }
+
+          // Process Time
+          let endTime = new Date();
+          console.log(`Start At: ${startTime}`)
+          console.log(`End At: ${endTime}`)
+          const diffMinute = Math.ceil(Math.abs(endTime - startTime) / (1000 * 60)); 
+          console.log(`Elapsed: ${diffMinute} minute`)
+
+          // Show Result
+          console.log(`Process amount: ${objResult.amountTotal}`);
+          if (objResult.listMergedCard.length > 0) {
+            console.log(`*** Merged Card ***`);
+            objResult.listMergedCard.map((card) => {
+              console.log(Object.values(card).toString());
+            })
+          }
+          if (objResult.listFail.length > 0) {
+            console.log(`*** Failed card ***`);
+            objResult.listFail.map((card) => {
+              console.log(Object.values(card).toString());
+            })
+          }
+
+          res.end(JSON.stringify(payload, null, 2));
+        } catch (error) {
+          console.log(error.message);
+          res.statusCode = 400;
+          res.end(error.message);
+        } finally {
+          if (browser) {
+            browser.close();
+          }
+        }
+      });
+
+      return;
+    }
+  }
+
+  // Default
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+  res.end(normalText);
+  return;
 });
     
 server.listen(port, hostname, () => {
